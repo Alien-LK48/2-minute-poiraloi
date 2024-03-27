55b1ff62fb5c9b2409cd351888d24e13837b242e
Alien-LK48 <164774532+Alien-LK48@users.noreply.github.com>
2024-03-27 17:26:29 +0600

    ce note

--- docs/ce/script.js
+++ docs/ce/script.js
@@ -50,7 +50,7 @@ var midsug=document.querySelector('.midsugg');
 var imgsofmidsuggs = ['bg.png', 'pdf.png'];
 
 var finalnotes = document.querySelector('.finalnote');
-var imagesofnote = ['coming soon.jpg',/*'bg.png', 'main_bg.png'*/];
+var imagesofnote = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
 
 var finalsuggus = document.querySelector('.finalsugg');
 var imagesofsuggs = ['no suggstion.jpg'];

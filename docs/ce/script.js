b3d9b4c088bd9cbecac5c8210d7ae701d9def957
Alien-LK48 <164774532+Alien-LK48@users.noreply.github.com>
2024-03-27 17:34:41 +0600

    Update script.js

--- docs/ce/script.js
+++ docs/ce/script.js
@@ -50,7 +50,7 @@ var midsug=document.querySelector('.midsugg');
 var imgsofmidsuggs = ['bg.png', 'pdf.png'];
 
 var finalnotes = document.querySelector('.finalnote');
-var imagesofnote = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
+var imagesofnote = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg'];
 
 var finalsuggus = document.querySelector('.finalsugg');
 var imagesofsuggs = ['no suggstion.jpg'];

8c2a9abebdb47cfaa516dae9de9d1cb07189ccc4
Alien-LK48 <164774532+Alien-LK48@users.noreply.github.com>
2024-03-29 20:29:02 +0600

    lab

--- docs/1259/script.js
+++ docs/1259/script.js
@@ -1,27 +1,5 @@
 
-var mid= new Date("Mar 30, 2024 00:00:00").getTime();
 var final= new Date("May 14, 2024 00:00:00").getTime();
-var x = setInterval(() => {
-        var now=new Date().getTime();
-        var distance= mid-now;
-
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        document.getElementById('d').innerHTML=days;
-        document.getElementById('h').innerHTML=hours;
-        document.getElementById('m').innerHTML=minutes;
-        document.getElementById('s').innerHTML=seconds;
-        let hide= document.getElementById('mid');
-        if (distance <= 0){
-            clearInterval(x);
-            hide.classList.toggle('hidden')
-        }
-
-}, 1000);
-
 var y = setInterval(() => {
         var now=new Date().getTime();
         var distance= final-now;
